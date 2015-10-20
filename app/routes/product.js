fa9b04ad4054c57f16d358a76a40f0f9b862a483
yulongcs <yulongcs@qq.com>
2015-10-20 18:15:13 +0800

    create

---
+++ app/routes/product.js
@@ -0,0 +1,16 @@
+var models  = require('../models');
+var express = require('express');
+var router  = express.Router();
+
+router.post('/create', function(req, res) {
+  models.User.create({
+    username: req.body.username
+  }).then(function () {
+    res.redirect('/exchange/new');
+    res.redirect('/');
+  });
+});
+
+
+
+module.exports = router;
---
+++ config/routes.js
@@ -0,0 +1,23 @@
+﻿var routes = require('../app/routes/index');
+var users = require('../app/routes/users');
+
+module.exports = function(app) {
+    app.use('/', routes);
+    app.use('/users', users);
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    // error handler
+    // no stacktraces leaked to user unless in development environment
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: (app.get('env') === 'development') ? err : {}
+        });
+    });
+}
